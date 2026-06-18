2fbe514240c104d04a2698920db77d6d0ac1a2c9
Starkewe <Jeremiahroberts15@gmail.com>
2026-06-18 16:28:51 -0700

    Add full-region cone tutorial boss

--- scripts/abilities/boss_ability_factory.gd
+++ scripts/abilities/boss_ability_factory.gd
@@ -4,12 +4,15 @@ class_name BossAbilityFactory
 const DirectionalRegionCleaveScript := preload("res://scripts/abilities/directional_region_cleave.gd")
 
 const ABILITY_TARGET_REGION_CLOSE_CLEAVE := "target_region_close_cleave"
-
+const ABILITY_TARGET_REGION_FULL_CONE := "target_region_full_cone"
 
 static func create_ability_from_id(ability_id: String) -> BossAbility:
 	match ability_id:
 		ABILITY_TARGET_REGION_CLOSE_CLEAVE:
 			return create_target_region_close_cleave()
+
+		ABILITY_TARGET_REGION_FULL_CONE:
+			return create_target_region_full_cone()
 
 		_:
 			print("Unknown boss ability id:", ability_id)
@@ -23,7 +26,18 @@ static func create_target_region_close_cleave() -> BossAbility:
 	ability.affected_ranges = ["close"]
 
 	return ability
+	
+static func create_target_region_full_cone() -> BossAbility:
+	var ability := DirectionalRegionCleaveScript.new()
 
+	ability.ability_name = "Full Region Cone"
+	ability.windup_text = "The boss lines up a long cone!"
+	ability.impact_text = "The cone tears through the full region!"
+
+	ability.region_span_steps = 0
+	ability.affected_ranges = ["close", "mid", "far"]
+
+	return ability
 
 static func create_fallback_ability() -> BossAbility:
 	return create_target_region_close_cleave()
